bab03c8af79674495ecf5ee2f892901a5422aa6f
Alex Kolev <kolev@mtr-design.com>
2016-07-22 21:25:39 +0300

    Removing users model from the API.

--- rest/time_tracker/views/__init__.py
+++ rest/time_tracker/views/__init__.py
@@ -1,4 +1,3 @@
-from time_tracker.views.user import UserViewSet
 from time_tracker.views.company import CompanyViewSet
 from time_tracker.views.project import ProjectViewSet
 from time_tracker.views.profile import ProfileViewSet
