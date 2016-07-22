1af24771cb028ec2e50cc1f4a8dafe4f6d5f341e
Krasimir Nikolov <krasimir.nikolov@mtr-design.com>
2016-07-22 17:40:51 +0300

    Fixed PEP8

--- rest/features/environment.py
+++ rest/features/environment.py
@@ -2,13 +2,14 @@ import logging
 
 from selenium import webdriver
 
+
 def before_all(context):
-    selenium_logger = logging.getLogger(
-        'selenium.webdriver.remote.remote_connection')
+    selenium_logger = logging.getLogger('selenium.webdriver.remote.remote_connection')
     selenium_logger.setLevel(logging.WARN)
 
-    context.browser= webdriver.Firefox()
+    context.browser = webdriver.Firefox()
     context.browser.implicitly_wait(3)
+
 
 def after_all(context):
     context.browser.quit()
