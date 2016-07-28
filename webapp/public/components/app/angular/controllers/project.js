7bf030f36d53abc626323fe152467c3eda43bf7f
Svet <svet.penev@gmail.com>
2016-07-28 10:27:53 +0300

    projects page filters

--- webapp/public/components/app/angular/controllers/project.js
+++ webapp/public/components/app/angular/controllers/project.js
@@ -3,17 +3,28 @@
     angular
         .module('app')
         .controller('ProjectController', ProjectController);
-    ProjectController.$inject = ['$rootScope', '$location', 'FlashService', 'PageService', 'ProjectsService', 'TimeReportsService', '$routeParams'];
-    function ProjectController($rootScope, $location, FlashService, PageService, ProjectsService, TimeReportsService, $routeParams) {
+    ProjectController.$inject = ['$rootScope', '$location', 'FlashService', 'PageService', 'ProjectsService', 'ProfilesService', 'TimeReportsService', '$routeParams'];
+    function ProjectController($rootScope, $location, FlashService, PageService, ProjectsService, ProfilesService, TimeReportsService, $routeParams) {
         var c = this;
+        c.filter = filter;
+        var search = $location.search();
         c.removeItem = removeItem;
         c.getProject = [];
+        c.profiles = [];
+        c.filterData = {};
+        c.filterData.profile__id = (search.profile__id) ? search.profile__id : null;
+        c.filterData.project__id = $routeParams.id;
         c.getProjectTimeReports = [];
+        c.getTimeReportsTotalHours = [];
+        c.getTimeReportsProfiles = [];
         (function initController() {
             PageService.resetData();
             PageService.setHtmlTitle('Projects');
             PageService.setSlug('projects');
             loadProject($routeParams.id);
+            listTimeReportsProfiles();
+            listTimeReportsTotalHours();
+            listProfiles();
         })();
         function loadProject(id) {
             ProjectsService.GetProject(id)
@@ -31,7 +42,21 @@
                 });
         }
 
-         function removeItem(id) {
+        function listTimeReportsTotalHours() {
+            TimeReportsService.GetReportsTotalHoursByConditions(c.filterData)
+                .then(function (response) {
+                    c.getTimeReportsTotalHours = response;
+                });
+        }
+
+        function listTimeReportsProfiles() {
+            TimeReportsService.GetReportsProfilesByConditions(c.filterData)
+                .then(function (response) {
+                    c.getTimeReportsProfiles = response;
+                });
+        }
+
+        function removeItem(id) {
             var r = confirm("Are you sure that you want to delete this item?");
             if(r){
                 TimeReportsService.Delete(id, function (response) {
@@ -43,6 +68,17 @@
                    loadProject($routeParams.id);
                 });
             }
+        }
+
+        function listProfiles() {
+            ProfilesService.GetAll()
+                .then(function (response) {
+                    c.profiles = response;
+                });
+        }
+
+        function filter() {
+            $location.url('/projects/'+$routeParams.id+'/time-reports?'+$.param(c.filterData));
         }
     }
 })();
