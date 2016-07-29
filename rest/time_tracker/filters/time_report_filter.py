2e74deda22160b7a03cdcbfaa24d18bfce409c8b
Svet <svet.penev@gmail.com>
2016-07-29 17:06:05 +0300

    fix order

--- rest/time_tracker/filters/time_report_filter.py
+++ rest/time_tracker/filters/time_report_filter.py
@@ -21,9 +21,10 @@ class TimeReportFilter(filters.FilterSet):
 
     def filter_group_by(self, queryset, value):
         if value in self.FILTERS:
-            queryset = queryset.annotate(year=Func(F('date'), function='YEAR')).order_by('-year')
             if value == 'DAY' or value == 'MONTH':
                 queryset = queryset.annotate(month=Func(F('date'), function='MONTH')).order_by('-month')
             if value == 'DAY':
                 queryset = queryset.annotate(day=Func(F('date'), function='day')).order_by('-day')
+
+            queryset = queryset.annotate(year=Func(F('date'), function='YEAR')).order_by('-year')
         return queryset
