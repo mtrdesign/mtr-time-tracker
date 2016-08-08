6b87f6599a118161070ba55feab87f6d74617977
Svet <svet.penev@gmail.com>
2016-08-08 11:53:43 +0300

    Unit test controllers

--- webapp/public/components/app/angular/tests/unit/controllersSpec.js
+++ webapp/public/components/app/angular/tests/unit/controllersSpec.js
@@ -1,8 +1,25 @@
 describe("Unit: Testing Controllers", function () {
+    var $controller, $rootScope;
 
-        console.log(app);
-        // expect(AccountController).not.to.equal(null);
-        }
-    );
+    beforeEach(angular.mock.module('app'));
+    beforeEach(angular.mock.inject(function (_$controller_,  _$rootScope_) {
+        $controller = _$controller_;
+        /* Set profile */
+        $rootScope = _$rootScope_.$new();
+        $rootScope.globals.currentUser = {};
+        $rootScope.globals.currentUser.profile = {};
+    }));
+
+    /* Check controllers */
+    it('should have a HomeController controller', function () {
+        var controller = $controller('HomeController', {$scope: $rootScope});
+        expect(controller).toBeDefined();
+    });
+
+    it('should have a AccountController controller', function () {
+        var controller = $controller('AccountController', {$scope: $rootScope});
+        expect(controller).toBeDefined();
+    });
 
 });
+
