12564e4041589490bfa14aff411df49b4b632682
Svet <svet.penev@gmail.com>
2016-08-12 11:26:42 +0300

    add loader

--- webapp/public/components/app/angular/filters.js
+++ webapp/public/components/app/angular/filters.js
@@ -16,10 +16,10 @@
     app.filter("dateRange", function () {
         return function (items, month, year) {
             var d = new Date();
-            d.setFullYear(year, month-1, 1);
-            var df = d.setHours(0,0);
+            d.setFullYear(year, month - 1, 1);
+            var df = d.setHours(0, 0);
             d.setFullYear(year, month, 0);
-            var dt = d.setHours(23,59);
+            var dt = d.setHours(23, 59);
 
             var result = [];
             for (var i = 0; i < items.length; i++) {
@@ -32,4 +32,69 @@
             return result;
         };
     });
+
+    app.factory('httpLoaderInterceptor', ['$rootScope', function ($rootScope) {
+        // Active request count
+        var requestCount = 0;
+
+        function startRequest(config) {
+            // If no request ongoing, then broadcast start event
+            if (!requestCount) {
+                $rootScope.$broadcast('httpLoaderStart');
+            }
+
+            requestCount++;
+            return config;
+        }
+
+        function endRequest(arg) {
+            // No request ongoing, so make sure we don’t go to negative count
+            if (!requestCount)
+                return;
+
+            requestCount--;
+            // If it was last ongoing request, broadcast event
+            if (!requestCount) {
+                $rootScope.$broadcast('httpLoaderEnd');
+            }
+
+            return arg;
+        }
+
+        // Return interceptor configuration object
+        return {
+            'request': startRequest,
+            'requestError': endRequest,
+            'response': endRequest,
+            'responseError': endRequest
+        };
+    }]);
+
+    app.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.interceptors.push('httpLoaderInterceptor');
+    }]);
+
+    app.directive('httpLoader', function () {
+        return {
+            restrict: 'EA',
+            link: function (scope, element) {
+                // Store original display mode of element
+                var shownType = element.css('display');
+                console.log(element);
+                function hideElement() {
+                    element.css('display', 'none');
+                }
+
+                scope.$on('httpLoaderStart', function () {
+                    element.css('display', shownType);
+                });
+
+                scope.$on('httpLoaderEnd', hideElement);
+
+                // Initially hidden
+                hideElement();
+            }
+        };
+    });
+
 })();
