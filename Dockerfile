9345619bcc9d5d2a40306b0796981ee7a96bd73c
kalostoykov <kalo.stoykov@protonmail.com>
2017-06-15 15:19:28 +0300

    set django docker container to run gunicorn

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ RUN mkdir /code
 WORKDIR /code
 ADD requirements.txt /code/
 RUN pip install -r requirements.txt
+RUN pip install gunicorn
 ADD . /code/
 ENTRYPOINT ["./migrate-run.sh"]
--- migrate-run.sh
+++ migrate-run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-python3 rest/manage.py migrate --settings=rest.env.local >> /dev/stdout
-python3 rest/manage.py runserver 0:8000 --settings=rest.env.local >> /dev/stdout
+python3 rest/manage.py migrate --settings=rest.env.local
+cd rest
+gunicorn -c gunicornconf.py rest.wsgi:application --env DJANGO_SETTINGS_MODULE="rest.env.local"
